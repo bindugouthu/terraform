23ca1ede4e7fd8d691facbf40c7e80c128c292af
bindugouthu <38145939+bindugouthu@users.noreply.github.com>
2022-01-09 19:06:26 +0000

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "sg" {
+  vpc_id      = "vpc-af5e76c7"
+  name = "github"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description      = "ssh from anywhere"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
